84edd273f20a7eeea136bfb766eb6ff806f8544c
johnbane85 <junyu.siew@gmail.com>
2022-12-11 11:39:12 +0800

    initial commit

---
+++ src/main/java/vttp/paf/day23/models/Brewery.java
@@ -0,0 +1,30 @@
+package vttp.paf.day23.models;
+
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+public class Brewery {
+
+  private String name;
+
+  public Brewery() {
+  };
+
+  public Brewery(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public static Brewery create(SqlRowSet rs) {
+    final Brewery brewery = new Brewery();
+    brewery.setName(rs.getString("name"));
+    return brewery;
+  }
+
+}
---
+++ src/main/java/vttp/paf/day23/services/BeerService.java
@@ -0,0 +1,45 @@
+package vttp.paf.day23.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import vttp.paf.day23.models.BeerStyle;
+import vttp.paf.day23.models.Brewery;
+import vttp.paf.day23.repositories.BreweryCache;
+import vttp.paf.day23.repositories.BreweryRepository;
+import vttp.paf.day23.repositories.StylesRepository;
+
+@Service
+public class BeerService {
+
+  @Autowired
+  private StylesRepository stylesRepo;
+
+  @Autowired
+  private BreweryRepository breweryRepo;
+
+  @Autowired
+  private BreweryCache breweryCache;
+
+  public List<BeerStyle> getBeerStyles() {
+    return stylesRepo.getStyles();
+  }
+
+  public List<Brewery> getBreweriesByBeerStyle(Integer beerStyle) {
+
+    // check if the result is in the cache. If it is, then use the list from Redis
+    Optional<List<Brewery>> opt = breweryCache.getBreweriesByBeerStyle(beerStyle);
+    if (opt.isPresent())
+      return opt.get();
+
+    // Query the database and cache the result
+    List<Brewery> breweries = breweryRepo.getBreweriesByStyle(beerStyle);
+    breweryCache.saveBreweriesByBeerStyle(beerStyle, breweries);
+
+    return breweries;
+  }
+
+}
